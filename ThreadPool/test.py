c6d8d9654ef08ae9a2a5f1bce986f1761766f603
Michael Yang <yangxing20107@gmail.com>
2020-05-27 13:56:23 +0800

    Update test.py

--- ThreadPool/test.py
+++ ThreadPool/test.py
@@ -17,7 +17,7 @@ result_list = pool.map(_queryAllResourceHandler, args)
 for i in result_list:
     print(i)
     
-# ThreadPoolExecutor的map函数的使用：
-#   result_list里面的结果出来的顺序是和args参数里面进去的顺序保持一致的 !!
+# ThreadPoolExecutor的map函数的使用： map返回的结果是有序结果；是根据迭代函数执行顺序返回的结果.
+#   也就是说，result_list里面的结果出来的顺序是和args参数里面进去的顺序保持一致的 !!
 
 
