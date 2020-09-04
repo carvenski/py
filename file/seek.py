8f4acd09dd2873bdcf3b8b322601c41b5bb7d044
yangxing <yangxing@mmtrix.com>
2020-09-04 11:17:25 +0800

    .

--- file/seek.py
+++ file/seek.py
@@ -12,18 +12,26 @@ from_what: It defines point of reference.
     2: sets the reference point at the end of the file
     By default from_what argument is set to 0.
 
+注意: 
+在文本模式(r)下,from_what必须为0,指针只能从文件开头向后移动offset,
+而在二进制模式(rb)下则没有限制,指针可以从开头/当前位置/末尾随意移动offset.
+所以显然,读磁盘数据时,为了灵活使用指针移动offset,一般都是使用rb模式打开文件.
 """
-with open('z', 'w') as f:
-    for i in range(100):
-        f.write(str(i))
+with open('z', 'wb') as f:
+    for i in range(255):
+        f.write( bytes([i]) )
 
 with open('z', 'rb') as f:
+    f.seek(0, 2)
+    print("file length is %s"  % f.tell())
     f.seek(0, 0)
-    print(f.tell())
-    f.seek(100, 0)
-    print(f.tell())
-    f.seek(-5, 2)
-    print(f.tell())
+    print("cursor in %s"  % f.tell())
+    f.seek(10, 0)
+    print("seek(10, 0), cursor in %s"  % f.tell())
+    f.seek(10, 1)
+    print("seek(10, 1), cursor in %s"  % f.tell())
+    f.seek(-10, 2)
+    print("seek(-10, 2), cursor in %s"  % f.tell())
 
 
 
