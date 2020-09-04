3a232b596aace9ae95f362df58554bca871f10a9
yangxing <yangxing@mmtrix.com>
2020-09-04 10:48:39 +0800

    .

--- file/seek.py
+++ file/seek.py
@@ -1,16 +1,28 @@
 """
-use seek()/tell() to move cursor in file.
+use seek()/tell() to move/get cursor in file.
+
+Function seek():
+Syntax: f.seek(offset, from_what), where f is file pointer
+
+Parameters:
+Offset: Number of postions to move forward
+from_what: It defines point of reference.
+    0: sets the reference point at the beginning of the file
+    1: sets the reference point at the current file position
+    2: sets the reference point at the end of the file
+    By default from_what argument is set to 0.
+
 """
 with open('z', 'w') as f:
     for i in range(100):
         f.write(str(i))
 
-with open('z') as f:
+with open('z', 'rb') as f:
     f.seek(0, 0)
     print(f.tell())
     f.seek(100, 0)
     print(f.tell())
-    f.seek(0, 2)
+    f.seek(-5, 2)
     print(f.tell())
 
 
