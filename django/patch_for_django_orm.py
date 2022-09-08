2fd8f035eb0b0ec6b0105fdeb109b62e11849694
Michael Yang <yangxing20107@gmail.com>
2022-09-08 09:13:40 +0800

    Update patch_for_django_orm.py

--- django/patch_for_django_orm.py
+++ django/patch_for_django_orm.py
@@ -19,10 +19,11 @@ https://code.djangoproject.com/ticket/9878
 # =========
 # Solution:
 
-First replace django orm's db engine by SQLAlchemy connection pool,
+First replace django orm's mysql database engine by SQLAlchemy connection pool,
 Then we need to add put-connection-back-into-pool action when user thread is finished.
 To implement above two features in django in a beautiful way, I make this patch. 
-If any suggestions, please contact xing.yang@intel.com. Lets make django better toghther.
+If any suggestions, please contact xing.yang@intel.com.
+Let's make django more beautiful.
 
 # ===============
 # What To Patch ?
@@ -37,12 +38,21 @@ patch django.db.backends.mysql.base.
 # ===============
 # Tested versions
 
-python = 3.7
-django = 3.2.15
+python = 3.7/2.7
+django = 3.2/1.9
 sqlalchemy = 1.4
-pymysql = 1.0
+pymysql = 1.0.2 (python3)
+MySQL-python = 1.2.5 (python2)
 mysql = 5.7
 uwsgi = 2.0
+
+# ====
+# Todo
+
+Now this patch is only for mysql database in django,
+PostgreSQL and other databases will be supported later.
+And only patch threading module in python2/3,
+If you use old thread/_thread module to create thread, no patch.
 """
 
 import threading
@@ -51,10 +61,10 @@ from django.db.backends.mysql import base
 from django.db.backends.mysql.base import DatabaseWrapper
 from sqlalchemy import pool
 
-def patch_all():
-    print(PATCH_EXPLAIN)
+def patch_all(**kw):
+    # print(PATCH_EXPLAIN)
     patch_thread()
-    patch_database()
+    patch_django_mysql_engine(**kw)
 
 def patch_thread():
     print("=== patch threading.Thread")
@@ -82,29 +92,34 @@ def patch_thread():
     threading.Thread.start = patch_Thread_start(threading._old_start)
 
 
-def patch_database():
-    print("=== patch base.Database")
-    
+def patch_django_mysql_engine(**kw):
+    print("=== patch django.db.backends.mysql")
+    default_pool_kw = {'poolclass': pool.QueuePool, 
+        'pool_size': 5, 'max_overflow': 0, 'timeout': 10,
+        'recycle': 1800, } #'pre_ping': True
+    default_pool_kw.update(kw)
+
     # patch django.db.backends.mysql.base to use sqlalchemy conn pool
+    print("=== init sqlalchemy conn pool, size=[%s]" % default_pool_kw['pool_size'])
     base._old_Database = base.Database
-    base.Database = pool.manage(
-        base._old_Database, 
-        poolclass=pool.QueuePool, 
-        pool_size=5, max_overflow=0, timeout=5,
-    )
-    print("=== init sqlalchemy conn pool, size= [5]")
+    base.Database = pool.manage(base._old_Database, **default_pool_kw)
 
     def patch_get_new_connection(func):
-        pool_conn_keys = {'charset', 'user', 'database', 'password', 'host', 'port', 'client_flag'}
+        pool_conn_keys = {'charset','user','db','database','passwd','password',
+            'host','port','client_flag','use_unicode'}
         def new_func(self, conn_params):
-            # patch conn_params because sqlalchemy pool connect serialization bug
+            # clean conn_params because sqlalchemy pools key serialization problem
             new_conn_params = {}
             for k in pool_conn_keys:
-                new_conn_params[k] = conn_params[k]
-            return func(self, new_conn_params)
+                if k in conn_params:
+                    new_conn_params[k] = conn_params[k]
+            # print("### sqlalchemy conn pools status= %s ###" % base.Database.pools)
+            connection = func(self, new_conn_params)
+            return connection
         return new_func
 
-    print("=== patch DatabaseWrapper.get_new_connection")
     DatabaseWrapper._old_get_new_connection = DatabaseWrapper.get_new_connection
     DatabaseWrapper.get_new_connection = patch_get_new_connection(DatabaseWrapper._old_get_new_connection)
+
+
 
